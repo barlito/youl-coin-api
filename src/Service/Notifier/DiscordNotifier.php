fea06e043e6eadd6732fea2c4880c142d82dbfdc
Axel Barlet <axelbarlet98@gmail.com>
2021-12-08 21:35:53 +0100

    wip

--- src/Service/Notifier/DiscordNotifier.php
+++ src/Service/Notifier/DiscordNotifier.php
@@ -9,9 +9,9 @@ use App\Money\YoulCoinFormatter;
 use DateTime;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Notifier\Bridge\Discord\DiscordOptions;
+use Symfony\Component\Notifier\Bridge\Discord\Embeds\DiscordAuthorEmbedObject;
 use Symfony\Component\Notifier\Bridge\Discord\Embeds\DiscordEmbed;
 use Symfony\Component\Notifier\Bridge\Discord\Embeds\DiscordFieldEmbedObject;
-use Symfony\Component\Notifier\Bridge\Discord\Embeds\DiscordFooterEmbedObject;
 use Symfony\Component\Notifier\ChatterInterface;
 use Symfony\Component\Notifier\Exception\LogicException;
 use Symfony\Component\Notifier\Exception\TransportExceptionInterface;
@@ -36,25 +36,26 @@ class DiscordNotifier
                 ->avatarUrl($this->discordOptionsParams['transaction']['avatar_url'])
                 ->addEmbed(
                     (new DiscordEmbed())
-                    ->title($this->discordOptionsParams['transaction']['success_title'])
-                    ->color($this->discordOptionsParams['transaction']['success_color'])
-                    ->timestamp(new DateTime())
-                    ->addField(
-                        (new DiscordFieldEmbedObject())
-                        ->name('-' . $this->youlCoinFormatter->format($transaction->getAmount()))
-                        ->value("<@{$transaction->getWalletFrom()->getDiscordUser()->getDiscordId()}>")
-                        ->inline(true),
-                    )
-                    ->addField(
-                        (new DiscordFieldEmbedObject())
-                        ->name('+' . $this->youlCoinFormatter->format($transaction->getAmount()))
-                        ->value("<@{$transaction->getWalletTo()->getDiscordUser()->getDiscordId()}>")
-                        ->inline(true),
-                    )
-                    ->footer(
-                        (new DiscordFooterEmbedObject())
-                        ->iconUrl($this->discordOptionsParams['transaction']['avatar_url']),
-                    ),
+                        ->title($this->discordOptionsParams['transaction']['success_title'])
+                        ->author(
+                            (new DiscordAuthorEmbedObject())
+                                ->iconUrl($this->discordOptionsParams['transaction']['avatar_url'])
+                                ->name($this->discordOptionsParams['transaction']['username']),
+                        )
+                        ->color($this->discordOptionsParams['transaction']['success_color'])
+                        ->timestamp(new DateTime())
+                        ->addField(
+                            (new DiscordFieldEmbedObject())
+                                ->name('-' . $this->youlCoinFormatter->format($transaction->getAmount()))
+                                ->value("<@{$transaction->getWalletFrom()->getDiscordUser()->getDiscordId()}>")
+                                ->inline(true),
+                        )
+                        ->addField(
+                            (new DiscordFieldEmbedObject())
+                                ->name('+' . $this->youlCoinFormatter->format($transaction->getAmount()))
+                                ->value("<@{$transaction->getWalletTo()->getDiscordUser()->getDiscordId()}>")
+                                ->inline(true),
+                        ),
                 )
             ;
 
@@ -75,23 +76,24 @@ class DiscordNotifier
                 ->avatarUrl($this->discordOptionsParams['transaction']['avatar_url'])
                 ->addEmbed(
                     (new DiscordEmbed())
-                    ->title($this->discordOptionsParams['transaction']['error_title'])
-                    ->color($this->discordOptionsParams['transaction']['error_color'])
-                    ->timestamp(new DateTime())
-                    ->addField(
-                        (new DiscordFieldEmbedObject())
-                        ->name('Error on transaction')
-                        ->value($errorMessage),
-                    )
-                    ->addField(
-                        (new DiscordFieldEmbedObject())
-                        ->name('Message content')
-                        ->value($messageContent),
-                    )
-                    ->footer(
-                        (new DiscordFooterEmbedObject())
-                        ->iconUrl($this->discordOptionsParams['transaction']['avatar_url']),
-                    ),
+                        ->title($this->discordOptionsParams['transaction']['error_title'])
+                        ->author(
+                            (new DiscordAuthorEmbedObject())
+                                ->iconUrl($this->discordOptionsParams['transaction']['avatar_url'])
+                                ->name($this->discordOptionsParams['transaction']['username']),
+                        )
+                        ->color($this->discordOptionsParams['transaction']['error_color'])
+                        ->timestamp(new DateTime())
+                        ->addField(
+                            (new DiscordFieldEmbedObject())
+                                ->name('Error on transaction')
+                                ->value($errorMessage),
+                        )
+                        ->addField(
+                            (new DiscordFieldEmbedObject())
+                                ->name('Message content')
+                                ->value($messageContent),
+                        ),
                 )
             ;
 
