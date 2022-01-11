952b5fa9e3ab19f2132f08b032831e3f832733a8
Axel BARLET <axelbarlet16@gmail.com>
2022-01-12 00:15:54 +0100

    Update utils.mk

--- make/utils.mk
+++ make/utils.mk
@@ -7,6 +7,10 @@ doctrine_migrate:
 	docker exec -t $(app_container_id) bin/console doctrine:database:create --if-not-exists
 	docker exec -t $(app_container_id) bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
+doctrine_migrate_ci:
+	docker exec -t $(app_container_id) bin/console doctrine:database:create --if-not-exists --env=test
+	docker exec -t $(app_container_id) bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration --env=test
+
 doctrine_reset_db:
 	docker exec -t $(app_container_id) bin/console doctrine:database:drop --force --if-exists
 	docker exec -t $(app_container_id) bin/console doctrine:database:create --if-not-exists
