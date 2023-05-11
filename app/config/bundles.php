cca92f4da623b2799a7b86c4e490fb1d4f217966
Axel Barlet <axelbarlet16@gmail.com>
2023-05-11 22:52:34 +0200

    feat: install debug

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -19,4 +19,6 @@ return [
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
